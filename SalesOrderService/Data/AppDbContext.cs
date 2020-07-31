32570161e87627dd0215697fa0db11a7b0efab3e
matt-r-mck <matt.r.mck@gmail.com>
2020-07-31 14:29:36 -0400

    Add project files.

---
+++ SalesOrderService/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesOrderService.Models;
+
+namespace SalesOrderService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesOrderService.Models.Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+
+    }
+}
---
+++ SalesOrderService/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesOrderService.Models {
+    public class Customer {
+
+        //[]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public decimal Sales { get; set; }
+
+        public Customer() {
+
+        }
+
+    }
+}
